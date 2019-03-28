fdbe9152a0f0e0a091f8c7be13b9504e36b707f2
Alex <ayang.passion@outlook.com>
2019-03-28 15:28:39 -0400

    Create game.js
    Added Sudo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+// Model:
+// Create ansewr object which include string of answer and it's associated character array, and stores the length of the array.
+
+// Game object keeps track of what character has previously been typed in.
+// Houses an answer object taken from answer object library
+// Keeps track of how many attempts are left.
+
+// Create ansewr object library, a list of names of popular modern games
+
+
+// Controller:
+// Show how many letters or spaces are in the answer
+// Game controller keeps a loop of games
+
+// View:
+// index.html
+// style.css
